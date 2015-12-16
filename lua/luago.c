0e9a4b8489aa600f301fdaffa1b695033866e608
redstorm-fyy <very_fyy@163.com>
2015-12-16 19:40:36 +0800

    modify

--- lua/luago.c
+++ lua/luago.c
@@ -1,6 +1,40 @@
 
 #include "_cgo_export.h"
 
-int finishpcallk(lua_State* L,int status,lua_KContext ctx){
-	return finishPCallk(L,status,ctx);
+int luago_finishpcallk(lua_State* L,int status,lua_KContext ctx){
+	return finishpcallk(L,status,ctx);
+}
+
+static int luago_gofunccall(lua_State* L){
+	return gofunccall(L);
+}
+
+static int luago_gofuncgc(lua_State* L){
+	return gofuncgc(L);
+}
+
+void luago_initstate(lua_State* L){
+	luaL_newmetatable(L,"__gofunc");		// meta
+	lua_pushcclosure(L,luago_gofuncgc,0);	// meta gc
+	lua_setfield(L,-2,"__gc");				// (meta.__gc=gc) meta
+	lua_pop(L,1);
+}
+
+void luago_pushgoclosure(lua_State* L,int ref){
+	int* udata=(int*)lua_newuserdata(L,sizeof(ref));// ref
+	*udata=ref;
+	luaL_setmetatable(L,"__gofunc");
+	lua_pushcclosure(L,luago_gofunccall,1);// call
+}
+
+int luago_togoclosure(lua_State* L,int idx){
+	int* udata=(int*)lua_touserdata(L,idx);
+	if(udata){
+		return *udata;
+	}
+	return -1;
+}
+
+int luago_upvalueindex(int idx){
+	return lua_upvalueindex(idx);
 }
--- lua/luago.go
+++ lua/luago.go
@@ -6,7 +6,11 @@ package lua
 #include "lualib.h"
 #include "lauxlib.h"
 
-int finishpcallk(lua_State* L,int status,lua_KContext ctx);
+int luago_finishpcallk(lua_State* L,int status,lua_KContext ctx);
+void luago_initstate(lua_State* L);
+void luago_pushgoclosure(lua_State* L,int ref);
+int luago_togoclosure(lua_State* L,int idx);
+int luago_upvalueindex(int idx);
 
 */
 import "C"
@@ -29,10 +33,11 @@ func NewState() *State {
 	C.lua_pushlightuserdata(L, unsafe.Pointer(L))
 	C.lua_pushlightuserdata(L, unsafe.Pointer(s))
 	C.lua_settable(L, C.LUA_REGISTRYINDEX)
+	C.luago_initstate(L)
 	return s
 }
 
-func getState(L *C.lua_State) *State {
+func getstate(L *C.lua_State) *State {
 	C.lua_pushlightuserdata(L, unsafe.Pointer(L))
 	C.lua_gettable(L, C.LUA_REGISTRYINDEX)
 	s := C.lua_touserdata(L, -1)
@@ -48,26 +53,63 @@ func (s *State) OpenLibs() {
 	C.luaL_openlibs(s.s)
 }
 
-func (s *State) regkfunc(call func(*State, int)) int {
+func (s *State) LoadFile(fname string) int {
+	cname := C.CString(fname)
+	defer C.free(unsafe.Pointer(cname))
+	return int(C.luaL_loadfilex(s.s, cname, nil))
+}
 
+func (s *State) LoadString(str string) int {
+	cs := C.CString(str)
+	defer C.free(unsafe.Pointer(cs))
+	return int(C.luaL_loadstring(s.s, cs))
+}
+
+func (s *State) LoadBuffer(buf []byte) int {
+	return int(C.luaL_loadbufferx(s.s, (*C.char)(unsafe.Pointer(&buf[0])), C.size_t(len(buf)), nil, nil))
+}
+
+type GoClosure func(s *State) int
+
+func (s *State) register(obj interface{}) uint {
+	freelen := len(s.freeidx)
+	if freelen == 0 {
+		s.reg = append(s.reg, obj)
+		return uint(len(s.reg)) - 1
+	}
+	idx := s.freeidx[freelen-1]
+	s.reg[idx] = obj
+	s.freeidx = s.freeidx[0 : freelen-1]
+	return idx
+}
+
+func (s *State) unregister(idx uint) {
+	if s.reg[idx] != nil {
+		s.reg[idx] = nil
+		s.freeidx = append(s.freeidx, idx)
+	}
+}
+
+func (s *State) getreg(idx uint) interface{} {
+	return s.reg[idx]
 }
 
 func (s *State) PCall(nargs int) int {
 	return int(C.lua_pcallk(s.s, C.int(nargs), C.LUA_MULTRET, 0, 0, nil))
 }
 
-//export finishPCallk
-func finishPCallk(L *C.lua_State, status C.int, ctx C.lua_KContext) int {
-	s := getState(L)
-	kfunc := s.getkfunc(ctx)
-	s.unregkfunc(ctx)
+//export finishpcallk
+func finishpcallk(L *C.lua_State, status C.int, ctx C.lua_KContext) int {
+	s := getstate(L)
+	kfunc := s.getreg(uint(ctx)).(func(*State, int) int)
+	s.unregister(uint(ctx))
 	return kfunc(s, int(status))
 }
 
-func (s *State) PCallk(nargs int, call func(*State, int)) int {
-	idx := s.regkfunc(call)
-	status := int(C.lua_pcallk(s.s, C.int(nargs), C.LUA_MULTRET, 0, idx, C.lua_KFunction(C.finishpcallk)))
-	s.unregkfunc(idx)
+func (s *State) PCallk(nargs int, call func(*State, int) int) int {
+	ref := s.register(call)
+	status := int(C.lua_pcallk(s.s, C.int(nargs), C.LUA_MULTRET, 0, C.lua_KContext(ref), C.lua_KFunction(C.luago_finishpcallk)))
+	s.unregister(ref)
 	return status
 }
 
@@ -128,15 +170,117 @@ func (s *State) typename(idx int) string {
 	return C.GoString(C.lua_typename(s.s, C.lua_type(s.s, C.int(idx))))
 }
 
-func (s *State) PushValue(v interface{}) {
-
+//export gofunccall
+func gofunccall(L *C.lua_State) int {
+	ref := C.luago_togoclosure(L, C.luago_upvalueindex(1))
+	if ref >= 0 {
+		s := getstate(L)
+		goclosure := s.getreg(uint(ref)).(GoClosure)
+		return goclosure(s)
+	}
+	return 0
 }
 
-func (s *State) ToValue(idx int) interface{} {
+//export gofuncgc
+func gofuncgc(L *C.lua_State) int {
+	ref := C.luago_togoclosure(L, 1)
+	if ref >= 0 {
+		s := getstate(L)
+		s.unregister(uint(ref))
+	}
+	return 0
+}
+
+func (s *State) pushgoclosure(v GoClosure) {
+	ref := s.register(v)
+	C.luago_pushgoclosure(s.s, C.int(ref))
+}
+
+func (s *State) togoclosure(idx int) GoClosure {
+	if C.lua_getupvalue(s.s, C.int(idx), 1) != nil {
+		ref := C.luago_togoclosure(s.s, -1)
+		s.Pop(1)
+		if ref >= 0 {
+			return s.getreg(uint(ref)).(GoClosure)
+		}
+	}
 	return nil
 }
 
-func (s *State) Call(fname string, args ...interface{}) ([]interface{}, error) {
+func (s *State) pushstring(str string) {
+	cs := C.CString(str)
+	defer C.free(unsafe.Pointer(cs))
+	C.lua_pushstring(s.s, cs)
+}
+
+func (s *State) PushValue(v interface{}) {
+	switch v := v.(type) {
+	default:
+		C.lua_pushnil(s.s)
+	case nil:
+		C.lua_pushnil(s.s)
+	case bool:
+		if v {
+			C.lua_pushboolean(s.s, 1)
+		} else {
+			C.lua_pushboolean(s.s, 0)
+		}
+	case string:
+		s.pushstring(v)
+	case []byte:
+		C.lua_pushlstring(s.s, (*C.char)(unsafe.Pointer(&v[0])), C.size_t(len(v)))
+	case float32:
+		C.lua_pushnumber(s.s, C.lua_Number(v))
+	case float64:
+		C.lua_pushnumber(s.s, C.lua_Number(v))
+	case int:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case uint:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case int32:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case uint32:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case int64:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case uint64:
+		C.lua_pushinteger(s.s, C.lua_Integer(v))
+	case GoClosure:
+		s.pushgoclosure(v)
+	}
+}
+
+func (s *State) ToValue(idx int) interface{} {
+	switch s.ltype(idx) {
+	default:
+		return nil
+	case C.LUA_TNIL:
+		return nil
+	case C.LUA_TBOOLEAN:
+		b := C.lua_toboolean(s.s, C.int(idx))
+		if b == 0 {
+			return false
+		} else {
+			return true
+		}
+	case C.LUA_TNUMBER:
+		n := C.lua_tonumberx(s.s, C.int(idx), nil)
+		i := C.lua_tointegerx(s.s, C.int(idx), nil)
+		if float64(n) == float64(i) {
+			return int64(i)
+		} else {
+			return float64(n)
+		}
+	case C.LUA_TSTRING:
+		var len C.size_t
+		cs := C.lua_tolstring(s.s, C.int(idx), &len)
+		return C.GoStringN(cs, C.int(len))
+	case C.LUA_TFUNCTION:
+		return s.togoclosure(idx)
+	}
+}
+
+func (s *State) Call(fname string, args []interface{}, call func(*State, []interface{}, error)) ([]interface{}, error) {
 	top := C.lua_gettop(s.s)
 	funcSplit := strings.Split(fname, ".")
 	s.GetGlobal(funcSplit[0])
@@ -153,18 +297,43 @@ func (s *State) Call(fname string, args ...interface{}) ([]interface{}, error) {
 		s.Remove(-2)
 	}
 	if s.ltype(-1) != C.LUA_TFUNCTION {
-		err := errors.New(s.typename(-1) + strings.Join(funcSplit, "."))
+		err := errors.New(s.typename(-1) + fname)
 		s.Pop(1)
 		return nil, err
 	}
 	for _, arg := range args {
 		s.PushValue(arg)
 	}
-	status := C.lua_pcallk(s.s, C.int(len(args)), C.LUA_MULTRET, 0, 0, nil)
-	var rets []interface{}
-	for i, n := int(top)+1, int(C.lua_gettop(s.s)); i <= n; i++ {
-		rets = append(rets, s.ToValue(i))
+	setrets := func(s *State, oldtop int) []interface{} {
+		var rets []interface{}
+		for i, n := oldtop+1, int(C.lua_gettop(s.s)); i <= n; i++ {
+			rets = append(rets, s.ToValue(i))
+		}
+		return rets
 	}
-	C.lua_settop(s.s, top)
-	return rets, nil
+	var status int
+	if call != nil {
+		aftercall := func(s *State, status int) int {
+			if status != C.LUA_OK && status != C.LUA_YIELD {
+				C.lua_settop(s.s, top)
+				call(s, nil, errors.New("yield call "+fname))
+			} else {
+				rets := setrets(s, int(top))
+				C.lua_settop(s.s, top)
+				call(s, rets, nil)
+			}
+			return 0
+		}
+		status = s.PCallk(len(args), aftercall)
+	} else {
+		status = s.PCall(len(args))
+	}
+	if status != C.LUA_OK && status != C.LUA_YIELD {
+		C.lua_settop(s.s, top)
+		return nil, errors.New("call " + fname)
+	} else {
+		rets := setrets(s, int(top))
+		C.lua_settop(s.s, top)
+		return rets, nil
+	}
 }
